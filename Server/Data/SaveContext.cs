200263e0a6e6f4fb238b3db3251de0041cec57fa
brittatoresson <britta_toresson@hotmail.com>
2023-02-10 12:37:44 +0100

    #1 add post to db

---
+++ Server/Data/SaveContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Examensarbete.Shared.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examensarbete.Server.Data
+{
+    public class SaveContext : DbContext
+    {
+        public SaveContext()
+        {
+        }
+
+        public SaveContext(DbContextOptions<SaveContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkoutModel>();
+        }
+        //oklart vad denna gör
+        public DbSet<WorkoutModel> Workout { get; set; }
+
+    }
+}
