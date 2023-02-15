1a779e4c6cbcb3ac7c525fd25f05c0805f0c857c
brittatoresson <britta_toresson@hotmail.com>
2023-02-15 14:49:58 +0100

    #5 update routing, workoutSummery, css

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,7 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-//Serssion stoeagre
+//Serssion storage
 builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddBlazoredSessionStorageAsSingleton();
 
